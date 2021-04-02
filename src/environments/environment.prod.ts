87a5a61fbcc5190bfd02e4d06701a2a00f259066
erick_sm3 <erick_sm3@hotmail.com>
2021-04-02 11:32:50 -0300

    a

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiKey: '2e0c80a79d0a173229b4844111e9fccb'
+  apiKey: '2e0c80a79d0a173229b4844111e9fccb',
+  baseUrl: 'https://api.openweathermap.org/data/2.5'
 };
